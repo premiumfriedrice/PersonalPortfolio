0df6eae338eea67aab53e96abae8b098d36804f1
premiumfriedrice <lloyddalba@gmail.com>
2023-10-14 14:31:40 -0500

    added functionality for mobile testimonial scrolling

---
+++ app.js
@@ -0,0 +1,30 @@
+var btn = document.getElementsByClassName("btn");
+var slide = document.getElementById("slide")
+
+let transAmount = "translateX(-56.75rem)"
+
+var vpsize = window.matchMedia("(max-width: 768px)")
+
+function changeTranslate(x) {
+    if (x.matches) { // If media query matches translate amount is change, this needed because if the translateamount is set to normal in a smaller viewport, overscroll will happen
+        transAmount = "translateX(-17.05rem)"; 
+        }
+}
+
+changeTranslate(vpsize) //
+
+btn[0].onclick = function() {
+    slide.style.transform = "translateX(0px)";
+    for(i=0;i<2;i++){
+        btn[i].classList.remove("active")
+    }
+    this.classList.add("active")
+}
+
+btn[1].onclick = function() {
+    slide.style.transform = transAmount;
+    for(i=0;i<2;i++){
+        btn[i].classList.remove("active")
+    }
+    this.classList.add("active")
+}  
