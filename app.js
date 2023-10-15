d00468bd2470352b87a660eb250c80125a88a237
premiumfriedrice <lloyddalba@gmail.com>
2023-10-14 19:02:51 -0500

    fixed mobile testimonial scroll for iphone 15 pro viewport

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var slide = document.getElementById("slide")
 
 let transAmount = "translateX(-56.75rem)"
 
-var vpsize = window.matchMedia("(max-width: 600px)")
+var vpsize = window.matchMedia("(max-width: 400px)")
 
 function changeTranslate(x) {
     if (x.matches) { // If media query matches translate amount is change, this needed because if the translateamount is set to normal in a smaller viewport, overscroll will happen
-        transAmount = "translateX(-17.05rem)"; 
+        transAmount = "translateX(-19.95rem)"; 
         }
 }
 
